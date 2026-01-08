3cb539ab798763df1afabb52c3b0bfe28ac8b60d
Elijah Saint Pierre <esaintpierre@student.neumont.edu>
2026-01-08 06:36:04 -0500

    field foe basic move to player

--- Scripts/Field/field_foe.gd
+++ Scripts/Field/field_foe.gd
@@ -1,37 +1,46 @@
 extends CharacterBody3D
 
 var speed  = randf_range(2.0, 4.0)
-@export var model = Node3D
-@export var area: Area3D
-
-@onready var anim_tree = %AnimationTree
+var anim_tree: AnimationTree
+var encounters: Array[String]
+var model: Node3D
 
 var player
+var foe_id
+var spawner
 
 func _ready() -> void:
-	for body in area.get_overlapping_bodies():
-		if body.name == "FieldChar":
-			player = body
+	model = get_node("Model")
+	anim_tree = model.get_node("AnimationTree")
 
 func _physics_process(delta: float) -> void:
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, Vector3(0, 3, 9))
+	query.exclude = [self]
+	var result = space_state.intersect_ray(query)
+	
+	if (result && result["collider"].name == "FieldChar" && !player):
+		player = result["collider"]
+	else:
+		player = null
+	
 	velocity = Vector3.ZERO
 	if player:
 		var direction = global_position.direction_to(player.global_position)
 		direction.y = 0.0
 		velocity = direction * speed
-		model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
-
-		if velocity == Vector3.ZERO:
-			anim_tree.set("paramters/iwr_blend/blend_amount", -1.0) #a
-		else:
-			anim_tree.set("paramters/iwr_blend/blend_amount", 1.0) #a
+		rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
+	
+	if velocity == Vector3.ZERO:
+		anim_tree.set("paramters/iwr_blend/blend_amount", -1.0) 
+	else:
+		anim_tree.set("paramters/iwr_blend/blend_amount", 1.0) 
 	
 	move_and_slide()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("Works")
 	if body.name == "FieldChar":
-		player = body
-
-func _on_area_3d_body_exited(body: Node3D) -> void:
-	if body.name == "FieldChar":
-		player = null
+		#startbattle
+		print("Encounter!")
+		pass
--- Scripts/Field/field_spawner.gd
+++ Scripts/Field/field_spawner.gd
@@ -1,13 +1,29 @@
 extends Node3D
 
 @export var spawn_node: PackedScene
+@export var type: Spawner_Type
+@export var encounters: Array[String]
 
 #what dict looks like {"id": ["alive", "0,0,0"]}
 var spawn_id: String
 var spawn_active: bool
 var spawn_pos: Vector3
 
-func spawn(new_pos: Vector3 = self.position):
+enum Spawner_Type {
+	ITEM,
+	FOE,
+}
+
+func spawn(new_pos: Vector3 = position):
 	var new_spawn = spawn_node.instantiate()
 	add_child(new_spawn)
 	new_spawn.global_position = new_pos
+	new_spawn.foe_id = spawn_id
+	new_spawn.spawner = self
+	new_spawn.encounters = encounters
+
+	match (type):
+		Spawner_Type.ITEM:
+			pass
+		Spawner_Type.FOE:
+			pass
