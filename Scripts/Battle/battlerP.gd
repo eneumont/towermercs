572d85b872e0def153369bc0e1baba258d5213f5
Elijah Saint Pierre <esaintpierre@student.neumont.edu>
2026-02-23 10:08:21 -0500

    slight bug fix

--- Scripts/Battle/battlerP.gd
+++ Scripts/Battle/battlerP.gd
@@ -13,7 +13,39 @@ var classType: CharData.ClassType
 func create(is_player: bool, c_data: CharData = null, f_data: EnemyData = null, control: Controller = Controller.PLAYER1) -> void:
 	super(is_player, c_data, f_data, control)
 	classType = c_data.class_type
+
+func _ready() -> void:
+	cm.bm = bm
 	
+	set_sprites()
+
+func start_turn():
+	super()
+	command.visible = true
+
+func end_turn():
+	super()
+	cm.flip = false
+	command.visible = false
+
+func _process(delta: float) -> void:
+	if command.visible:
+		#research more to see if can do match
+		if (Input.is_action_just_released("square")):
+			cm.key_click("square")
+		elif (Input.is_action_just_released("circle")):
+			cm.key_click("circle")
+		elif (Input.is_action_just_released("cross")):
+			cm.key_click("cross")
+		elif Input.is_action_just_released("triangle"):
+			cm.key_click("triangle")
+		elif (Input.is_action_just_released("left_bumper")):
+			cm.key_click("left_bump")
+		elif (Input.is_action_just_released("right_bumper")):
+			cm.key_click("right_bump")
+
+
+func set_sprites(anim := "idle"):
 	match classType:
 		CharData.ClassType.KNIGHT:
 			anim_sprite.sprite_frames = load("res://Images/Sprites/Battle/knight_battle.tres")
@@ -44,32 +76,4 @@ func create(is_player: bool, c_data: CharData = null, f_data: EnemyData = null, 
 		CharData.ClassType.WITCH:
 			anim_sprite.sprite_frames = load("res://Images/Sprites/Battle/witch_battle.tres")
 			
-	anim_sprite.play("idle")
-
-func _ready() -> void:
-	cm.bm = bm
-
-func start_turn():
-	super()
-	command.visible = true
-
-func end_turn():
-	super()
-	cm.flip = false
-	command.visible = false
-
-func _process(delta: float) -> void:
-	if command.visible:
-		#research more to see if can do match
-		if (Input.is_action_just_released("square")):
-			cm.key_click("square")
-		elif (Input.is_action_just_released("circle")):
-			cm.key_click("circle")
-		elif (Input.is_action_just_released("cross")):
-			cm.key_click("cross")
-		elif Input.is_action_just_released("triangle"):
-			cm.key_click("triangle")
-		elif (Input.is_action_just_released("left_bumper")):
-			cm.key_click("left_bump")
-		elif (Input.is_action_just_released("right_bumper")):
-			cm.key_click("right_bump")
+	anim_sprite.play(anim)
